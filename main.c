6497dc2e6906b140ed31ff469485893eddd8edb4
kevin.vece <dfy.vece@gmail.com>
2015-04-06 16:31:48 -0500

    Added buzzer control functions

--- main.c
+++ main.c
@@ -26,6 +26,12 @@ fclose(f);
 
 */
 
+MyRio_Dio A5;
+MyRio_Dio A7;
+NiFpga_Bool dio_A5;
+NiFpga_Bool dio_A7;
+
+
 typedef enum {
 
 	HAS_FIRE,
@@ -41,13 +47,20 @@ MyRio_Uart uart;
 // INPUT/OUTPUT
 
 int readSensor() {
-	return 0;
-}
-
-int triggerAlarm() {
+	dio_A5 = Dio_ReadBit(&A5);
+	return dio_A5;
 
 }
 
+void alarmOn() {
+	dio_A7 = 1;
+	Dio_WriteBit(&A7, dio_A7);
+}
+
+void alarmOff() {
+	dio_A7 = 0;
+	Dio_WriteBit(&A7, dio_A7);
+}
 
 
 // NETWORK
@@ -55,7 +68,7 @@ int triggerAlarm() {
 int sendAlarm() {
 
 	while (state == HAS_FIRE) {
-		sendCommand(&uart, "ATNH 1");
+		//sendCommand(&uart, "ATNH 1");
 		sendPayload(&uart, "10");
 		sleep(10);
 	}
@@ -105,74 +118,68 @@ int main(int argc, char **argv) {
 
     char readData[BUFF_SIZE];
 
+
+    /****************************************************/
+    /************ Setup Digital Input/Output ************/
+    NiFpga_Status stat;
+
+	A5.dir = NiFpga_MyRio1900Fpga20_ControlU8_DIOA_70DIR;
+	A5.out = NiFpga_MyRio1900Fpga20_ControlU8_DIOA_70OUT;
+	A5.in = NiFpga_MyRio1900Fpga20_IndicatorU8_DIOA_70IN;
+	A5.bit = 5;
+
+	A7.dir = NiFpga_MyRio1900Fpga20_ControlU8_DIOA_70DIR;
+	A7.out = NiFpga_MyRio1900Fpga20_ControlU8_DIOA_70OUT;
+	A7.in = NiFpga_MyRio1900Fpga20_IndicatorU8_DIOA_70IN;
+	A7.bit = 7;
+
+	MyRio_Open();
+
+	/****************************************************/
+
     //char writeData[BUFF_SIZE];
+
+
 
 
     state = NORMAL;
     status = initUART(&uart);
 
-    pthread_t poll_thread;
-    pthread_create(&poll_thread, NULL, pollNeighbors, NULL);
+    //pthread_t poll_thread;
+    //pthread_create(&poll_thread, NULL, pollNeighbors, NULL);
 
     while (status >= VI_SUCCESS) {
 
-    	if ( readSensor() ) {
+    	if ( !readSensor() ) {								// alarm is on
     		if (state == NORMAL) {
 				state = HAS_FIRE;
 				pthread_t send_thread;
 				pthread_create(&send_thread, NULL, sendAlarm, NULL);
+				alarmOn();
     		}
     	}
-    	else {
-    		state = NORMAL;
-    	}
+    	//else {
+    		//state = NORMAL;
+    	//}
+
 
     	recvPayload(&uart, readData);
+    	if (strlen(readData)) printf("Received: '%s'\n", readData);
     	if (strlen(readData) && readData[0] == '1') {		// check for fire message
-    		triggerAlarm();
+    		printf("Turning on alarm\n");
+    		alarmOn();
     	}
 
     }
 
     status = closeUART(&uart);
+
+
+    MyRio_Close();
     return status;
 }
 
 
 
-
-
-/*
-NiFpga_Status stat;
-NiFpga_Bool dio_A0;
-NiFpga_Bool dio_A7;
-
-MyRio_Dio A0;
-MyRio_Dio A7;
-
-A0.dir = NiFpga_MyRio1950Fpga20_ControlU8_DIOA_70DIR;
-A0.out = NiFpga_MyRio1950Fpga20_ControlU8_DIOA_70OUT;
-A0.in = NiFpga_MyRio1950Fpga20_IndicatorU8_DIOA_70IN;
-A0.bit = 0;
-
-A7.dir = NiFpga_MyRio1950Fpga20_ControlU8_DIOA_70DIR;
-A7.out = NiFpga_MyRio1950Fpga20_ControlU8_DIOA_70OUT;
-A7.in = NiFpga_MyRio1950Fpga20_IndicatorU8_DIOA_70IN;
-A7.bit = 7;
-
-status = MyRio_Open();
-  if (MyRio_IsNotSuccess(status))
-  {
-      return status;
-  }
-
-  dio_A0 = Dio_ReadBit(&A0);
-
-
-  dio_A7 = 1;
-  Dio_WriteBit(&A7, dio_A7);
-
-  status = MyRio_Close();
-*/
 
 
